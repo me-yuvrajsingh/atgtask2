30a6a5658b1ea01c1727ae344c7bfe907dae96ff
me-yuvrajsingh <90550128+me-yuvrajsingh@users.noreply.github.com>
2023-12-29 23:15:32 +0530

    mobile.js

--- mobile.js
+++ mobile.js
@@ -1,26 +1,31 @@
 let dot = document.querySelectorAll(".dot");
 let items = document.querySelectorAll(".item");
+let actDotNo = 0;
 $(document).ready(function () {
   var owl = $(".owl-carousel");
   owl.owlCarousel({
     items: 1,
     loop: true,
     nav: false,
-    dots: false,
+    dots: true,
+    dotsContainer: '#dots',
+    dotsEach: true,
+    autoplay:true,
+    autoplayTimeout:1000,
   });
 
 
   // Add touch event listeners
-  owl.on("drag.owl.carousel", function (event) {
-    $(".dot").removeClass("active");
-  });
+  // owl.on("drag.owl.carousel", function (event) {
+  //   $(".dot").removeClass("active");
+  // });
 
-  owl.on("changed.owl.carousel", function (event) {
-    var current = event.item.index;
-    var dots = $(".dot");
-    dots.removeClass("active");
-    dots.eq(current % dots.length).addClass("active");
-  });
+  // owl.on("changed.owl.carousel", function (event) {
+  //   var current = event.item.index;
+  //   var dots = $(".dot");
+  //   dots.removeClass("active");
+  //   dots.eq(current % dots.length).addClass("active");
+  // });
   $(".dots-indicator .dot").click(function () {
     var index = $(this).index();
     owl.trigger("to.owl.carousel", [index, 300]);
@@ -29,19 +34,25 @@ $(document).ready(function () {
   });
 });
 
-console.log(dot[0])
-function def(){
-  for(let i=0; i<dot.length; i++){
-    if(dot[i].classList.contains("active")){
-      dot[i].classList.remove("active");
-    }
-  }
-  for(i=0;i<items.length;i++){
-    if(items[i].classList.contains("act-item")){
-      items[i].classList.remove("act-item");
-    }
-  }
-}
+
+
+// setInterval(() => {
+//   // autoSlide(actDotNo);
+// }, 4000);
+
+// console.log(dot[0])
+// function def(){
+//   for(let i=0; i<dot.length; i++){
+//     if(dot[i].classList.contains("active")){
+//       dot[i].classList.remove("active");
+//     }
+//   }
+  // for(i=0;i<items.length;i++){
+  //   if(items[i].classList.contains("act-item")){
+  //     items[i].classList.remove("act-item");
+  //   }
+  // }
+// }
 
 function actdot(e){
   def();
